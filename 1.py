a7bffc6d217eb678a4b1d747c577ba351281507a
TaRa1999R <rezaietara78@gmail.com>
2023-08-05 11:38:25 +0330

    assignment8

--- 1.py
+++ 1.py
@@ -1,13 +1,13 @@
-# print (" MAKING GIF ")
+print (" MAKING GIF ")
 
-# import os
-# import imageio
+import os
+import imageio
 
-# image_list = sorted ( os.listdir ("image1") )
-# IMAGE = []
+image_list = sorted ( os.listdir ("image1") )
+IMAGE = []
 
-# for name in image_list :
-#     image_path = "image1/" + name
-#     IMAGE.append (imageio.imread (image_path))
+for name in image_list :
+    image_path = "image1/" + name
+    IMAGE.append (imageio.imread (image_path))
 
-# imageio.mimsave ("image1/first.gif", IMAGE)
+imageio.mimsave ("image1/first.gif", IMAGE)
